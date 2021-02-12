71eed431affc48a3009dc68bffcca01af5774333
Jo Franz <franz@systopia.de>
2021-02-12 11:14:32 +0100

    Added parameter type for addFile()

--- CRM/Civioffice/DocumentStore/LocalTemp.php
+++ CRM/Civioffice/DocumentStore/LocalTemp.php
@@ -42,9 +42,8 @@ class CRM_Civioffice_DocumentStore_LocalTemp extends CRM_Civioffice_DocumentStor
      *
      * @return CRM_Civioffice_Document_LocalTempfile
      *   new temp file
-     *
      */
-    public function addFile($file_name, $content = null) : CRM_Civioffice_Document_LocalTempfile
+    public function addFile(string $file_name, $content = null) : CRM_Civioffice_Document_LocalTempfile
     {
         $file_path = $this->base_folder . DIRECTORY_SEPARATOR . $file_name;
         return new CRM_Civioffice_Document_LocalTempfile($this, $this->mime_type, $file_path);
