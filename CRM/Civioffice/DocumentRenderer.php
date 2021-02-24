e6fd4a12cfae2dffe6edae69b2c206b7126d5d60
B. Endres <endres@systopia.de>
2021-02-24 15:52:04 +0100

    added token replacement code

--- CRM/Civioffice/DocumentRenderer.php
+++ CRM/Civioffice/DocumentRenderer.php
@@ -43,12 +43,70 @@ abstract class CRM_Civioffice_DocumentRenderer extends CRM_Civioffice_OfficeComp
     public abstract function render(array $documents, string $target_mime_type) : array;
 
 
+    /**
+     * resolve all tokens
+     * @param array $token_names
+     *   the list of all token names to be replaced
+     *
+     * @param integer $entity_id
+     *   entity ID, e.g. contact_id
+     *
+     * @param string $entity_type
+     *   entity type, e.g. 'contact'
+     *
+     * @return array
+     *   list of token_name => token value
+     */
     public function resolveTokens($token_names, $entity_id, $entity_type = 'contact') : array {
-        return [];
+        // TODO: implement
+        // TODO: use additional token system
+        throw new Exception('resolveTokens not implemented');
     }
 
+    /**
+     * Replace all tokens with {token_name} and {$smarty_var.attribute} format
+     *
+     * @param array $token_names
+     *   the list of all token names to be replaced
+     *
+     * @param integer $entity_id
+     *   entity ID, e.g. contact_id
+     *
+     * @param string $entity_type
+     *   entity type, e.g. 'contact'
+     *
+     * @return array
+     *   list of token_name => token value
+     *
+     * @throws \Exception
+     */
     public function replaceAllTokens($string, $entity_id, $entity_type = 'contact') : string {
-        return $string;
+        // TODO: use additional token system
+        $additional_tokens = [];
+
+        if ($entity_type == 'contact') {
+            // first: replace CiviCRM style contact tokens
+            [$contact] = CRM_Utils_Token::getTokenDetails([$entity_id]);
+            $string = CRM_Utils_Token::replaceContactTokens(
+                $string,
+                $contact[$entity_id],
+                false,
+                $additional_tokens
+            );
+
+            // second: replace SMARTY style tokens
+            /* @var CRM_Core_Smarty $smarty */
+            $smarty = CRM_Core_Smarty::singleton();
+            foreach ($additional_tokens as $key => $value) {
+                $smarty->assign($key, $value);
+            }
+            $string = $smarty->fetch("string:$string");
+
+            return $string;
+        }
+
+        // todo: implement?
+        throw new Exception('replaceAllTokens not implemented for entity ' . $entity_type);
     }
 
 }
