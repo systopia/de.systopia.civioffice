78121cdf3d59c18c01a79dc0309f4d17e02e6593
Jo Franz <franz@systopia.de>
2021-07-12 18:35:00 +0200

    Add empty boilerplate code for new contact page

---
+++ CRM/Civioffice/Page/DocumentFromSingleContact.php
@@ -0,0 +1,16 @@
+<?php
+use CRM_Civioffice_ExtensionUtil as E;
+
+class CRM_Civioffice_Page_DocumentFromSingleContact extends CRM_Core_Page {
+
+  public function run() {
+    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    CRM_Utils_System::setTitle(E::ts('DocumentFromSingleContact'));
+
+    // Example: Assign a variable for use in a template
+    $this->assign('currentTime', date('Y-m-d H:i:s'));
+
+    parent::run();
+  }
+
+}
