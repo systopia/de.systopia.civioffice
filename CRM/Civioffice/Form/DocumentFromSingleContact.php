096db6f535250e06156d382ee50ac50bde3f7650
B. Endres <endres@systopia.de>
2021-09-16 17:24:49 +0200

    removed duplicate setTitle

--- CRM/Civioffice/Form/DocumentFromSingleContact.php
+++ CRM/Civioffice/Form/DocumentFromSingleContact.php
@@ -30,8 +30,6 @@ class CRM_Civioffice_Form_DocumentFromSingleContact extends CRM_Core_Form {
 
         $this->assign('user_id', $this->contact_id);
 
-        CRM_Utils_System::setTitle(E::ts('Document creation for single contact')); // fixme duplicated?
-
         $this->setTitle(E::ts("CiviOffice - Generate Document"));
 
         $config = CRM_Civioffice_Configuration::getConfig();
