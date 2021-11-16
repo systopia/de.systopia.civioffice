ddaaf4b9d4e46bed5a6c8bd98ffc24769dddead5
Jens Schuppe <schuppe@systopia.de>
2021-11-16 11:12:13 +0100

    Check for interface to exist before instantiating implementing class

--- civioffice.php
+++ civioffice.php
@@ -45,7 +45,9 @@ function civioffice_civicrm_config(&$config)
 {
     _civioffice_civix_civicrm_config($config);
 
-    \Civi::dispatcher()->addSubscriber(new CRM_Civioffice_AttachmentProvider());
+    if (interface_exists('\Civi\Mailbatch\AttachmentType\AttachmentTypeInterface')) {
+        \Civi::dispatcher()->addSubscriber(new CRM_Civioffice_AttachmentProvider());
+    }
 }
 
 /**
