8e036dec307e00f68e43020dee8a9ed49d33b298
Jens Schuppe <schuppe@systopia.de>
2025-02-12 13:36:07 +0100

    Version 1.0-beta9

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.civioffice',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '25a6c0a6648cc9166ab5e12aca6fbd8836d8e61b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'beberlei/assert' => array(
+            'pretty_version' => 'v3.3.3',
+            'version' => '3.3.3.0',
+            'reference' => 'b5fd8eacd8915a1b627b8bfc027803f1939734dd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../beberlei/assert',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'systopia/de.systopia.civioffice' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '25a6c0a6648cc9166ab5e12aca6fbd8836d8e61b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
