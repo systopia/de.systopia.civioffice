d4e6a4eb913e478148db4c5f409dbcf926fb812c
Jens Schuppe <schuppe@systopia.de>
2025-03-25 13:39:13 +0100

    Only register search tasks when CiviOffice permission is granted

--- civioffice.php
+++ civioffice.php
@@ -1,7 +1,10 @@
 <?php
+
 declare(strict_types = 1);
 
+// phpcs:disable PSR1.Files.SideEffects
 require_once 'civioffice.civix.php';
+// phpcs:enable
 
 use CRM_Civioffice_ExtensionUtil as E;
 use Symfony\Component\Config\Resource\FileResource;
@@ -22,84 +25,101 @@ function _civioffice_composer_autoload(): void {
 /**
  * Implements hook_civicrm_container().
  */
-function civioffice_civicrm_container(ContainerBuilder $container): void
-{
-    _civioffice_composer_autoload();
+function civioffice_civicrm_container(ContainerBuilder $container): void {
+  _civioffice_composer_autoload();
 
-    $globResource = new GlobResource(__DIR__ . '/services', '/*.php', FALSE);
-    // Container will be rebuilt if a *.php file is added to services
-    $container->addResource($globResource);
-    foreach ($globResource->getIterator() as $path => $info) {
-        // Container will be rebuilt if file changes
-        $container->addResource(new FileResource($path));
-        require $path;
-    }
+  $globResource = new GlobResource(__DIR__ . '/services', '/*.php', FALSE);
+  // Container will be rebuilt if a *.php file is added to services
+  $container->addResource($globResource);
+  foreach ($globResource->getIterator() as $path => $info) {
+    // Container will be rebuilt if file changes
+    $container->addResource(new FileResource($path));
+    require $path;
+  }
 
-    if (function_exists('_civioffice_test_civicrm_container')) {
-        // Allow to use different services in tests.
-        _civioffice_test_civicrm_container($container);
-    }
+  if (function_exists('_civioffice_test_civicrm_container')) {
+    // Allow to use different services in tests.
+    _civioffice_test_civicrm_container($container);
+  }
 }
 
-function civioffice_civicrm_searchTasks($objectType, &$tasks)
-{
+/**
+ * Implements hook_civicrm_searchTasks().
+ *
+ * @phpstan-param array<string, array{"title": string, "class": string, "result": bool}> $tasks
+ */
+function civioffice_civicrm_searchTasks(string $objectType, array &$tasks): void {
+  if (CRM_Core_Permission::check('access CiviOffice')) {
     switch ($objectType) {
-        case 'contact':
-            $tasks['civioffice'] = [
-                'title' => E::ts('Create Documents (CiviOffice)'),
-                'class' => 'CRM_Civioffice_Form_Task_CreateDocuments',
-                'result' => false,
-            ];
-            break;
-        case 'contribution':
-            $tasks['civioffice'] = [
-                'title' => E::ts('Create Documents (CiviOffice)'),
-                'class' => 'CRM_Civioffice_Form_Task_CreateContributionDocuments',
-                'result' => false,
-            ];
-            break;
-        case 'event':
-            $tasks['civioffice'] = [
-                'title' => E::ts('Create Documents (CiviOffice)'),
-                'class' => 'CRM_Civioffice_Form_Task_CreateParticipantDocuments',
-                'result' => false,
-            ];
-            break;
-        case 'membership':
-            $tasks['civioffice'] = [
-                'title' => E::ts('Create Documents (CiviOffice)'),
-                'class' => 'CRM_Civioffice_Form_Task_CreateMembershipDocuments',
-                'result' => false,
-            ];
-            break;
-        case 'activity':
-            $tasks['civioffice'] = [
-                'title' => E::ts('Create Documents (CiviOffice)'),
-                'class' => 'CRM_Civioffice_Form_Task_CreateActivityDocuments',
-                'result' => false,
-            ];
-            break;
-        case 'case':
-            $tasks['civioffice'] = [
-                'title' => E::ts('Create Documents (CiviOffice)'),
-                'class' => 'CRM_Civioffice_Form_Task_CreateCaseDocuments',
-                'result' => false,
-            ];
+      case 'contact':
+        $tasks['civioffice'] = [
+          'title' => E::ts('Create Documents (CiviOffice)'),
+          'class' => 'CRM_Civioffice_Form_Task_CreateDocuments',
+          'result' => FALSE,
+        ];
+        break;
+
+      case 'contribution':
+        $tasks['civioffice'] = [
+          'title' => E::ts('Create Documents (CiviOffice)'),
+          'class' => 'CRM_Civioffice_Form_Task_CreateContributionDocuments',
+          'result' => FALSE,
+        ];
+        break;
+
+      case 'event':
+        $tasks['civioffice'] = [
+          'title' => E::ts('Create Documents (CiviOffice)'),
+          'class' => 'CRM_Civioffice_Form_Task_CreateParticipantDocuments',
+          'result' => FALSE,
+        ];
+        break;
+
+      case 'membership':
+        $tasks['civioffice'] = [
+          'title' => E::ts('Create Documents (CiviOffice)'),
+          'class' => 'CRM_Civioffice_Form_Task_CreateMembershipDocuments',
+          'result' => FALSE,
+        ];
+        break;
+
+      case 'activity':
+        $tasks['civioffice'] = [
+          'title' => E::ts('Create Documents (CiviOffice)'),
+          'class' => 'CRM_Civioffice_Form_Task_CreateActivityDocuments',
+          'result' => FALSE,
+        ];
+        break;
+
+      case 'case':
+        $tasks['civioffice'] = [
+          'title' => E::ts('Create Documents (CiviOffice)'),
+          'class' => 'CRM_Civioffice_Form_Task_CreateCaseDocuments',
+          'result' => FALSE,
+        ];
+        break;
     }
+  }
 }
 
-function civioffice_civicrm_summaryActions(&$actions, $contactID)
-{
-    // add "open document with single contact" action
-    $actions['open_document_with_single_contact'] = [
-        'ref'         => 'civioffice-render-single',
-        'title'       => E::ts('Create CiviOffice document'),
-        'weight'      => -110, // to the top!
-        'key'         => 'open_document_with_single_contact',
-        'class'       => 'medium-popup',
-        'href'        => CRM_Utils_System::url('civicrm/civioffice/document_from_single_contact', "reset=1"), // fixme contact id is passed twice as pid
-        'permissions' => ['access CiviOffice']
-    ];
+/**
+ * Implements hook_civicrm_summaryActions().
+ *
+ * @phpstan-param array<string, array<string, mixed>> $actions
+ */
+function civioffice_civicrm_summaryActions(array &$actions, ?int $contactID): void {
+  // add "open document with single contact" action
+  $actions['open_document_with_single_contact'] = [
+    'ref' => 'civioffice-render-single',
+    'title' => E::ts('Create CiviOffice document'),
+    // to the top!
+    'weight' => -110,
+    'key' => 'open_document_with_single_contact',
+    'class' => 'medium-popup',
+    // fixme contact id is passed twice as pid
+    'href' => CRM_Utils_System::url('civicrm/civioffice/document_from_single_contact', 'reset=1'),
+    'permissions' => ['access CiviOffice'],
+  ];
 }
 
 /**
@@ -107,82 +127,74 @@ function civioffice_civicrm_summaryActions(&$actions, $contactID)
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
  */
-function civioffice_civicrm_config(&$config)
-{
-    _civioffice_composer_autoload();
-    _civioffice_civix_civicrm_config($config);
+function civioffice_civicrm_config(\CRM_Core_Config &$config): void {
+  _civioffice_composer_autoload();
+  _civioffice_civix_civicrm_config($config);
 }
 
 /**
  * Implements hook_civicrm_permission().
- */
-function civioffice_civicrm_permission(&$permissions) {
-    $permissions['access CiviOffice'] = [
-        'label' => E::ts('Access CiviOffice'),
-        'description' => E::ts('Create documents using CiviOffice.'),
-    ];
-}
-
-// --- Functions below this ship commented out. Uncomment as required. ---
-
-/**
- * Implements hook_civicrm_preProcess().
  *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ * @phpstan-param array<string, array<string, mixed>> $permissions
  */
-//function civioffice_civicrm_preProcess($formName, &$form) {
-//
-//}
+function civioffice_civicrm_permission(array &$permissions): void {
+  $permissions['access CiviOffice'] = [
+    'label' => E::ts('Access CiviOffice'),
+    'description' => E::ts('Create documents using CiviOffice.'),
+  ];
+}
 
 /**
  * Implements hook_civicrm_navigationMenu().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ *
+ * @phpstan-param array<string, array<string, mixed>> $menu
  */
-function civioffice_civicrm_navigationMenu(&$menu) {
-    _civioffice_civix_insert_navigation_menu($menu, 'Administer/Communications', array(
-        'label' => E::ts('CiviOffice Settings'),
-        'name' => 'civioffice_settings',
-        'url' => 'civicrm/admin/civioffice/settings',
-        'permission' => 'administer CiviCRM',
-        'operator' => 'OR',
-        'separator' => 0,
-        'icon' => 'crm-i fa-file-text',
-    ));
-    _civioffice_civix_insert_navigation_menu($menu, '', array(
-        'label' => E::ts('CiviOffice'),
-        'name' => 'civioffice',
-        'permission' => 'access CiviOffice',
-        'operator' => 'OR',
-        'separator' => 0,
-        'icon' => 'crm-i fa-file-text',
-    ));
-    _civioffice_civix_insert_navigation_menu($menu, 'civioffice', array(
-        'label' => E::ts('CiviOffice Settings'),
-        'name' => 'civioffice_settings',
-        'url' => 'civicrm/admin/civioffice/settings',
-        'permission' => 'administer CiviCRM',
-        'operator' => 'OR',
-        'separator' => 0,
-        'icon' => 'crm-i fa-cogs',
-    ));
-    _civioffice_civix_insert_navigation_menu($menu, 'civioffice', array(
-        'label' => E::ts('Upload Documents'),
-        'name' => 'civioffice_document_upload',
-        'url' => 'civicrm/civioffice/document_upload',
-        'permission' => 'access CiviOffice',
-        'operator' => 'OR',
-        'separator' => 0,
-        'icon' => 'crm-i fa-upload',
-    ));
-    _civioffice_civix_insert_navigation_menu($menu, 'civioffice', array(
-        'label' => E::ts('Available Tokens'),
-        'name' => 'civioffice_tokens',
-        'url' => 'civicrm/civioffice/tokens',
-        'permission' => 'access CiviOffice',
-        'operator' => 'OR',
-        'separator' => 0,
-        'icon' => 'crm-i fa-code',
-    ));
+function civioffice_civicrm_navigationMenu(array &$menu): void {
+  _civioffice_civix_insert_navigation_menu($menu, 'Administer/Communications', [
+    'label' => E::ts('CiviOffice Settings'),
+    'name' => 'civioffice_settings',
+    'url' => 'civicrm/admin/civioffice/settings',
+    'permission' => 'administer CiviCRM',
+    'operator' => 'OR',
+    'separator' => 0,
+    'icon' => 'crm-i fa-file-text',
+  ]);
+  _civioffice_civix_insert_navigation_menu($menu, '', [
+    'label' => E::ts('CiviOffice'),
+    'name' => 'civioffice',
+    'permission' => 'access CiviOffice',
+    'operator' => 'OR',
+    'separator' => 0,
+    'icon' => 'crm-i fa-file-text',
+  ]);
+  _civioffice_civix_insert_navigation_menu($menu, 'civioffice', [
+    'label' => E::ts('CiviOffice Settings'),
+    'name' => 'civioffice_settings',
+    'url' => 'civicrm/admin/civioffice/settings',
+    'permission' => 'administer CiviCRM',
+    'operator' => 'OR',
+    'separator' => 0,
+    'icon' => 'crm-i fa-cogs',
+  ]);
+  _civioffice_civix_insert_navigation_menu($menu, 'civioffice', [
+    'label' => E::ts('Upload Documents'),
+    'name' => 'civioffice_document_upload',
+    'url' => 'civicrm/civioffice/document_upload',
+    'permission' => 'access CiviOffice',
+    'operator' => 'OR',
+    'separator' => 0,
+    'icon' => 'crm-i fa-upload',
+  ]);
+  _civioffice_civix_insert_navigation_menu($menu, 'civioffice', [
+    'label' => E::ts('Available Tokens'),
+    'name' => 'civioffice_tokens',
+    'url' => 'civicrm/civioffice/tokens',
+    'permission' => 'access CiviOffice',
+    'operator' => 'OR',
+    'separator' => 0,
+    'icon' => 'crm-i fa-code',
+  ]);
   _civioffice_civix_navigationMenu($menu);
 }
