69cb166f1150eda9d15bc124556bb104d265751c
Dominic Tubach <tubach@systopia.de>
2025-05-15 12:41:52 +0200

    Set version to 1.0.0-beta11

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.civioffice',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6c16735fc9db71c3206ebff97a955566c161ba3a',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'beberlei/assert' => array(
+            'pretty_version' => 'v3.3.3',
+            'version' => '3.3.3.0',
+            'reference' => 'b5fd8eacd8915a1b627b8bfc027803f1939734dd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../beberlei/assert',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'systopia/de.systopia.civioffice' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6c16735fc9db71c3206ebff97a955566c161ba3a',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
