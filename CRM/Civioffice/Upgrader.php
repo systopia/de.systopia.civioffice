6208d50961504a27380692c5945c38a385a70fa0
Jens Schuppe <schuppe@systopia.de>
2026-06-04 12:58:57 +0200

    Remove unnecessary code comment about path separators

--- CRM/Civioffice/Upgrader.php
+++ CRM/Civioffice/Upgrader.php
@@ -176,9 +176,6 @@ class CRM_Civioffice_Upgrader extends CRM_Extension_Upgrader_Base {
     );
 
     $config = CRM_Core_Config::singleton();
-    // $config->uploadDir / $config->customFileUploadDir are guaranteed to end
-    // with a directory separator, so concatenate directly as in
-    // CRM_Civioffice_DocumentStore_Upload.
     $legacy = $config->uploadDir . 'civioffice_documents';
     $target = $config->customFileUploadDir . 'civioffice_documents';
 
